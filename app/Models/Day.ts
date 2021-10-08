3b3a337b44d27526a3f9296447dacbea6d28a5ef
ghemsley <ghemsley@protonmail.ch>
2021-10-08 14:51:21 -0500

    Add Day model

---
+++ app/Models/Day.ts
@@ -0,0 +1,46 @@
+import { DateTime } from 'luxon'
+import { BaseModel, belongsTo, BelongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+
+export default class Day extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public userId: number
+
+  @column({ prepare: (value: number) => Math.max(0, Math.min(10, value)) })
+  public rating: number
+
+  @column({ prepare: (value: string) => value.toLowerCase() })
+  public mood: string
+
+  @column({ prepare: (value: number) => Math.max(0, value) })
+  public meals: number
+
+  @column()
+  public exercise: boolean
+
+  @column()
+  public meds: boolean
+
+  @column.dateTime({
+    autoCreate: true,
+    consume: (value: Date) => {
+      return value.valueOf()
+    },
+  })
+  public createdAt: DateTime
+
+  @column.dateTime({
+    autoCreate: true,
+    autoUpdate: true,
+    consume: (value: DateTime) => {
+      return value.valueOf()
+    },
+  })
+  public updatedAt: DateTime
+
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
+}
