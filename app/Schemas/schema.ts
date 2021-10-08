54a2739223046fcfbd136dfe848867eda5aa550a
ghemsley <ghemsley@protonmail.ch>
2021-10-08 11:25:29 -0500

    Set User schema date field to PositiveInt

--- app/Schemas/schema.ts
+++ app/Schemas/schema.ts
@@ -1,22 +1,23 @@
 import { gql } from 'apollo-server'
 
-const types = gql`
-  scalar DateTime
+const schema = gql`
+  scalar PositiveInt
 
   type User {
     id: Int
     username: String
     admin: Boolean
-    created: DateTime
-    updated: DateTime
+    createdAt: PositiveInt
+    updatedAt: PositiveInt
   }
 
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
+    user(id: Int, username: String, admin: Boolean, createdAt: Int, updatedAt: Int): User
     users: [User]
   }
 `
 
-export default types
+export default schema
