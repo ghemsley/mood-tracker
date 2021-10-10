e022ab1c60ba3f926c6d97c16eeb75b5016afc18
ghemsley <ghemsley@protonmail.ch>
2021-10-10 04:12:37 -0500

    Update home page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from 'next'
-import { useDispatch } from 'react-redux'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useDispatch, useSelector } from 'react-redux'
 import apiHooks from '../api'
 import actions from '../redux/actions'
 
 const Home: NextPage = () => {
-  return <div>Home</div>
+  const currentUser = useSelector((state) => state.user.currentUser)
+  const { data, error, loading } = apiHooks.useFetchUserById(currentUser.id, true)
+  error && console.error(error)
+
+  return <div>{JSON.stringify(data)}</div>
 }
 
 export default Home
