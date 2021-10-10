1d3a14d875a4825395eff934e44ec7f383dcab6d
ghemsley <ghemsley@protonmail.ch>
2021-10-10 04:10:45 -0500

    Update users api hooks

--- client/api/users.ts
+++ client/api/users.ts
@@ -1,31 +1,47 @@
-import { request } from 'graphql-request'
+import { GraphQLClient, request, gql } from 'graphql-request'
 import useSWR from 'swr'
 import constants from '../constants'
 
+const setToken = (token: string) => {
+  localStorage.setItem('token', token)
+}
+
+const getToken = () => {
+  return localStorage.getItem('token')
+}
+
 const userHooks = {
-  useFetchUserById: (id: number): { data: any; error: any; loading: boolean } => {
+  useFetchUserById: (id: number, start: boolean): { data: any; error: any; loading: boolean } => {
+    const token = getToken()
     const fetchUserById = (url: string) => {
       return request(
         url,
-        `{
-        user(id: ${id}) {
-          id
-          email
-          admin
-          createdAt
-          updatedAt
-        }
-      }`
+        gql`
+          {
+            user(id: ${id}) {
+              id
+              email
+              admin
+              createdAt
+              updatedAt
+            }
+          }
+        `,
+        null,
+        new Headers({ Authorization: `Bearer ${token}` })
       )
     }
-    const { data, error } = useSWR(constants.API, fetchUserById)
+    const { data, error } = useSWR(start ? constants.API + '/api' : null, fetchUserById)
     return {
       data: data,
       error: error,
       loading: !error && !data,
     }
   },
-  useFetchUserByEmail: (email: string): { data: any; error: any; loading: boolean } => {
+  useFetchUserByEmail: (
+    email: string,
+    start: boolean
+  ): { data: any; error: any; loading: boolean } => {
     const fetchUserByEmail = (url: string) => {
       return request(
         url,
@@ -40,20 +56,23 @@ const userHooks = {
       }`
       )
     }
-    const { data, error } = useSWR(constants.API, fetchUserByEmail)
+    const { data, error } = useSWR(start ? constants.API + '/api' : null, fetchUserByEmail)
     return {
       data: data,
       error: error,
       loading: !error && !data,
     }
   },
-  useFetchUsers: (args?: {
-    id?: number
-    email?: string
-    admin?: boolean
-    createdAt?: number
-    updatedAt?: number
-  }): { data: any; error: any; loading: boolean } => {
+  useFetchUsers: (
+    args?: {
+      id?: number
+      email?: string
+      admin?: boolean
+      createdAt?: number
+      updatedAt?: number
+    },
+    start?: boolean
+  ): { data: any; error: any; loading: boolean } => {
     const stringifyArgs = () => {
       if (args) {
         const array = []
@@ -77,7 +96,30 @@ const userHooks = {
       }`
       )
     }
-    const { data, error } = useSWR(constants.API, fetchUsers)
+    const { data, error } = useSWR(start ? constants.API + '/api' : null, fetchUsers)
+    return {
+      data: data,
+      error: error,
+      loading: !error && !data,
+    }
+  },
+  useLoginUser: (
+    email: string,
+    password: string,
+    start: boolean
+  ): { data: any; error: any; loading: boolean } => {
+    const loginUser = (url: string) =>
+      fetch(url, {
+        method: 'POST',
+        redirect: 'follow',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      }).then((response) => response.json())
+    const { data, error } = useSWR(start ? constants.API + '/login' : null, loginUser)
+    data && data.token && setToken(data.token)
     return {
       data: data,
       error: error,
