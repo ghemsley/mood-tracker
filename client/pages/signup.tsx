d498a2990d35db11defb5f7967206dc05a0a6684
ghemsley <ghemsley@protonmail.ch>
2021-10-10 04:12:23 -0500

    Update signup page

--- client/pages/signup.tsx
+++ client/pages/signup.tsx
@@ -3,27 +3,47 @@ import { FlexboxGrid, Panel, Form, ButtonToolbar, Button } from 'rsuite'
 import { useDispatch } from 'react-redux'
 import apiHooks from '../api'
 import actions from '../redux/actions'
+import { useState } from 'react'
 
-const Home: NextPage = () => {
+const Signup: NextPage = () => {
+  const [startFetch, setStartFetch] = useState(false)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
   const dispatch = useDispatch()
-  const { data, error, loading } = apiHooks.useFetchUserByEmail('user@example.com')
+  const { data, error, loading } = apiHooks.useSignupUser(
+    email,
+    password,
+    confirmPassword,
+    startFetch
+  )
+  const handleSubmit = (event: Event) => {
+    event.preventDefault()
+    setStartFetch(true)
+  }
+  if (!stop && !loading && !error) {
+    data && data.user && dispatch(actions.createUser(data.user))
+  }
   return (
     <FlexboxGrid justify="center">
       <FlexboxGrid.Item colspan={12}>
-        <Panel header={<h3>Login</h3>} bordered>
+        <Panel header={<h3>Sign up</h3>} bordered>
           <Form fluid>
             <Form.Group>
-              <Form.ControlLabel>Username or email address</Form.ControlLabel>
-              <Form.Control name="name" />
+              <Form.ControlLabel>Email address</Form.ControlLabel>
+              <Form.Control name="email" />
             </Form.Group>
             <Form.Group>
               <Form.ControlLabel>Password</Form.ControlLabel>
               <Form.Control name="password" />
             </Form.Group>
             <Form.Group>
+              <Form.ControlLabel>Confirm password</Form.ControlLabel>
+              <Form.Control name="confirmPassword" />
+            </Form.Group>
+            <Form.Group>
               <ButtonToolbar>
-                <Button appearance="primary">Sign in</Button>
-                <Button appearance="link">Forgot password?</Button>
+                <Button appearance="primary">Sign up</Button>
               </ButtonToolbar>
             </Form.Group>
           </Form>
@@ -32,3 +52,5 @@ const Home: NextPage = () => {
     </FlexboxGrid>
   )
 }
+
+export default Signup
