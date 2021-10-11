3412d3b04da47755fef7d9fcd478c42d27ef18e6
ghemsley <ghemsley@protonmail.ch>
2021-10-11 09:18:59 -0500

    Add api helpers

---
+++ client/api/helpers.ts
@@ -0,0 +1,23 @@
+import request, { gql } from 'graphql-request'
+
+const helpers = {
+  setToken: (token: string) => {
+    localStorage.setItem('token', token)
+  },
+
+  getToken: () => {
+    return localStorage.getItem('token')
+  },
+  stringifyArgs: (args: any) => {
+    if (args) {
+      const array = []
+      for (const [key, value] of Object.entries(args)) {
+        array.push(`${key}: ${typeof value === 'string' ? '"' + value + '"' : value}`)
+      }
+      return `(${array.join(', ')})`
+    } else return ''
+  },
+  auth: () => new Headers({ Authorization: `Bearer ${helpers.getToken()}` }),
+}
+
+export default helpers
