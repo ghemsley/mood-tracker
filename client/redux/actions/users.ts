8429352af52076320ecde19816e9d1148a881583
ghemsley <ghemsley@protonmail.ch>
2021-10-15 01:35:18 -0500

    Update user actions

--- client/redux/actions/users.ts
+++ client/redux/actions/users.ts
@@ -1,5 +1,18 @@
+import User from '../../models/user'
+
+interface UserObject {
+  id?: number
+  email?: string
+  password?: string
+  admin?: boolean
+  enabled?: string
+  createdAt?: number
+  updatedAt?: number
+}
+type Payload = User | UserObject
+
 const users = {
-  createUser: (payload: any) => ({ type: 'CREATE_USER', payload }),
+  createUser: (payload: Payload) => ({ type: 'CREATE_USER', payload }),
   logoutUser: () => ({ type: 'LOGOUT_USER' }),
 }
 
