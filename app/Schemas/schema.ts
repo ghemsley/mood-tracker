b35e9e103e07f64672a567e12d8158c4f4547d8c
ghemsley <ghemsley@protonmail.ch>
2021-10-15 01:31:51 -0500

    Update graphql schema

--- app/Schemas/schema.ts
+++ app/Schemas/schema.ts
@@ -4,49 +4,80 @@ const schema = gql`
   scalar PositiveInt
 
   type Day {
-    id: Int
-    userId: Int
+    id: Int!
+    userId: Int!
     rating: Int
     mood: String
     meals: Int
+    water: Int
+    people: Int
+    activities: Int
     exercise: Boolean
     meds: Boolean
-    createdAt: PositiveInt
-    updatedAt: PositiveInt
+    journal: String
+    createdAt: PositiveInt!
+    updatedAt: PositiveInt!
   }
 
   type User {
-    id: Int
-    email: String
-    admin: Boolean
-    createdAt: PositiveInt
-    updatedAt: PositiveInt
+    id: Int!
+    email: String!
+    admin: Boolean!
+    enabled: String!
+    createdAt: PositiveInt!
+    updatedAt: PositiveInt!
     days: [Day]
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    user(id: Int, email: String, admin: Boolean, createdAt: Int, updatedAt: Int): User
-    users(id: Int, email: String, admin: Boolean, createdAt: Int, updatedAt: Int): [User]
+    user(
+      id: Int
+      email: String
+      admin: Boolean
+      enabled: String
+      createdAt: Int
+      updatedAt: Int
+    ): User
+
+    users(
+      id: Int
+      email: String
+      admin: Boolean
+      enabled: String
+      createdAt: Int
+      updatedAt: Int
+    ): [User]
+
     day(
       id: Int
       userId: Int
       rating: Int
       mood: String
       meals: Int
+      water: Int
+      people: Int
+      activities: Int
       exercise: Boolean
       meds: Boolean
+      journal: String
+      createdAt: PositiveInt
+      updatedAt: PositiveInt
     ): Day
+
     days(
       id: Int
       userId: Int
       rating: Int
       mood: String
       meals: Int
+      water: Int
+      people: Int
+      activities: Int
       exercise: Boolean
       meds: Boolean
+      journal: String
+      createdAt: PositiveInt
+      updatedAt: PositiveInt
     ): [Day]
   }
 `
