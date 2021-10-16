e06e1bf4d5e1d6e5e93a7b653b68187a8f372d47
ghemsley <ghemsley@protonmail.ch>
2021-10-15 19:59:00 -0500

    Update resolvers

--- app/Resolvers/Resolvers.ts
+++ app/Resolvers/Resolvers.ts
@@ -10,20 +10,64 @@ const resolvers = {
   Query: {
     async user(parent, args, context, info) {
       const user = await getUser(context)
-      if (user && user.admin) {
+
+      if (user?.admin) {
         return User.query().preload('days').where(args).first()
       } else {
-        return User.find(user)
+        return user?.load('days')
       }
     },
-    users(parent, args, context, info) {
-      return User.query().preload('days').where(args)
+    async users(parent, args, context, info) {
+      const user = await getUser(context)
+      return user?.admin ? User.query().preload('days').where(args) : user?.load('days')
     },
-    day(parent, args, context, info) {
-      return Day.query().where(args).first()
+    async day(parent, args, context, info) {
+      const user = await getUser(context)
+      if (user?.admin) {
+        if (parent) {
+          return Day.query()
+            .where({ ...args, userId: parent.id })
+            .first()
+        } else {
+          return Day.query().where(args).first()
+        }
+      } else {
+        return user?.related('days').query().where(args).first()
+      }
     },
-    days(parent, args, context, info) {
-      return Day.query().where(args)
+    async days(parent, args, context, info) {
+      const user = await getUser(context)
+      if (user?.admin) {
+        if (parent) {
+          return Day.query().where({ ...args, userId: parent.id })
+        } else {
+          return Day.query().where(args)
+        }
+      } else {
+        return user?.related('days').query().where(args)
+      }
+    },
+  },
+  Mutation: {
+    async addUser(parent, args, context, info) {
+      if (args?.email && args?.password) {
+        return User.create(args)
+      } else throw new Error('User cannot be created without username and password arguments')
+    },
+    async addDay(parent, args, context, info) {
+      const user = await getUser(context)
+      if (user?.admin) {
+        const options = { ...args }
+        const dayUser = await User.find(options.userId)
+        delete options.userId
+        return dayUser?.related('days').create(options)
+      } else {
+        const options = { ...args }
+        if (options.userId) {
+          delete options.userId
+        }
+        return user?.related('days').create(options)
+      }
     },
   },
 }
