a2094ba57631781867dd61809ea11d4e75e97803
ghemsley <ghemsley@protonmail.ch>
2021-10-18 09:36:27 -0500

    Add fetcher helper to client

--- client/api/helpers.ts
+++ client/api/helpers.ts
@@ -1,5 +1,3 @@
-import request, { gql } from 'graphql-request'
-
 const helpers = {
   setToken: (token: string) => {
     localStorage.setItem('token', token)
@@ -21,6 +19,27 @@ const helpers = {
     } else return ''
   },
   auth: () => new Headers({ Authorization: `Bearer ${helpers.getToken()}` }),
+  fetcher: (
+    url: string,
+    method: 'GET' | 'PUT' | 'POST' | 'PATCH' | 'DELETE',
+    body?: object | null | undefined
+  ) => {
+    const token = helpers.getToken()
+    return fetch(url, {
+      method,
+      redirect: 'follow',
+      mode: 'cors',
+      headers: token
+        ? {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+          }
+        : {
+            'Content-Type': 'application/json',
+          },
+      body: body && JSON.stringify(body),
+    }).then((response) => response.json())
+  },
 }
 
 export default helpers
