4b7633ce9cdfcf58d72e612641ae6eefc8a14029
ghemsley <ghemsley@protonmail.ch>
2021-10-18 09:53:49 -0500

    Update client signup page

--- client/pages/signup.tsx
+++ client/pages/signup.tsx
@@ -1,49 +1,121 @@
 import type { NextPage } from 'next'
-import { FlexboxGrid, Panel, Form, ButtonToolbar, Button } from 'rsuite'
-import { useDispatch } from 'react-redux'
+import { FlexboxGrid, Panel, Form, ButtonToolbar, Button, Message, Loader } from 'rsuite'
+import { useDispatch, useSelector } from 'react-redux'
 import apiHooks from '../api'
 import actions from '../redux/actions'
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
+import { UserObject } from '../models/user'
+import { useRouter } from 'next/dist/client/router'
 
 const Signup: NextPage = () => {
+  const user: UserObject | undefined = useSelector((state) => state.user.currentUser)
+  const router = useRouter()
   const [startFetch, setStartFetch] = useState(false)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
-  const dispatch = useDispatch()
+  const [validationErrors, setValidationErrors] = useState(new Array<string>())
   const { data, error, loading } = apiHooks.useSignupUser(
     email,
     password,
     confirmPassword,
     startFetch
   )
-  const handleSubmit = (event: Event) => {
+  const dispatch = useDispatch()
+  const handleSubmit = (checkStatus: boolean, event: FormEvent) => {
     event.preventDefault()
-    setStartFetch(true)
+    let emailGood = false
+    let passwordCheckGood = false
+    let passwordLengthGood = false
+    if (/\w+@\w+\.\w+/i.test(email)) {
+      emailGood = true
+    } else
+      setValidationErrors((prev) => {
+        const message = 'Invalid email address'
+        return prev.find((element) => element === message) ? prev : [...prev, message]
+      })
+    if (password === confirmPassword) {
+      passwordCheckGood = true
+    } else
+      setValidationErrors((prev: string[]) => {
+        const message = 'Passwords do not match'
+        return prev.find((element) => element === message) ? prev : [...prev, message]
+      })
+    if (password.length >= 8) {
+      passwordLengthGood = true
+    } else
+      setValidationErrors((prev: string[]) => {
+        const message = 'Password length must be eight characters or more'
+        return prev.find((element) => element === message) ? prev : [...prev, message]
+      })
+    if (emailGood && passwordCheckGood && passwordLengthGood) setStartFetch(true)
   }
-  if (!stop && !loading && !error) {
-    data && data.user && dispatch(actions.createUser(data.user))
+  if (data?.user && !user?.id) {
+    dispatch(actions.setAuthenticated(data.user))
   }
-  return (
+  if (user?.id) {
+    router.push('/calendar')
+  }
+  if (error) {
+    console.log('error', error)
+  }
+  return loading ? (
+    <Loader center size="lg" content="loading..." />
+  ) : (
     <FlexboxGrid justify="center">
       <FlexboxGrid.Item colspan={12}>
         <Panel header={<h3>Sign up</h3>} bordered>
-          <Form fluid>
+          {(data?.errors || validationErrors.length > 0) && (
+            <Message showIcon closable type="error" header="Error">
+              {validationErrors.map(
+                (error: string, i: number) => error !== '' && <p key={i}>{error}</p>
+              )}
+              {data?.errors.map((error: { message: string }, i: number) => (
+                <p key={-i}>{error.message}</p>
+              ))}
+            </Message>
+          )}
+          <Form fluid onSubmit={handleSubmit}>
             <Form.Group>
               <Form.ControlLabel>Email address</Form.ControlLabel>
-              <Form.Control name="email" />
+              <Form.Control
+                name="email"
+                onChange={(value) => {
+                  setValidationErrors([])
+                  setStartFetch(false)
+                  setEmail(value)
+                }}
+              />
             </Form.Group>
             <Form.Group>
               <Form.ControlLabel>Password</Form.ControlLabel>
-              <Form.Control name="password" />
+              <Form.Control
+                name="password"
+                type="password"
+                onChange={(value) => {
+                  setValidationErrors([])
+                  setStartFetch(false)
+                  setPassword(value)
+                }}
+              />
             </Form.Group>
             <Form.Group>
               <Form.ControlLabel>Confirm password</Form.ControlLabel>
-              <Form.Control name="confirmPassword" />
+              <Form.Control
+                name="confirmPassword"
+                type="password"
+                onChange={(value) => {
+                  setValidationErrors([])
+                  setStartFetch(false)
+                  setConfirmPassword(value)
+                }}
+              />
             </Form.Group>
             <Form.Group>
               <ButtonToolbar>
-                <Button appearance="primary">Sign up</Button>
+                <Button appearance="primary" type="submit">
+                  Sign up
+                </Button>
               </ButtonToolbar>
             </Form.Group>
           </Form>
