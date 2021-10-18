431a51d845a3b53e3651d0b8a5cda70c7ce3a99e
ghemsley <ghemsley@protonmail.ch>
2021-10-18 09:41:47 -0500

    Add day redux actions

--- client/redux/actions/days.ts
+++ client/redux/actions/days.ts
@@ -1,3 +1,12 @@
-const days = {}
+import { Day, DayObject } from '../../models/day'
+
+const days = {
+  createDay: (payload: DayObject) => (dispatch: any) =>
+    Promise.resolve(dispatch({ type: 'CREATE_DAY', payload })),
+  createDays: (payload: DayObject[]) => (dispatch: any) =>
+    Promise.resolve(dispatch({ type: 'CREATE_DAYS', payload })),
+
+  clearDays: () => (dispatch: any) => Promise.resolve(dispatch({ type: 'CLEAR_DAYS' })),
+}
 
 export default days
