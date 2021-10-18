1f8183f1d1b136c142196cc22096ce2bb8f4d6bd
ghemsley <ghemsley@protonmail.ch>
2021-10-18 09:52:44 -0500

    Update client logout page

--- client/pages/logout.tsx
+++ client/pages/logout.tsx
@@ -1,14 +1,25 @@
 import type { NextPage } from 'next'
+import { useRouter } from 'next/dist/client/router'
+import { memo, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { FlexboxGrid, Loader } from 'rsuite'
 import apiHooks from '../api'
+import helpers from '../api/helpers'
 import actions from '../redux/actions'
+import { ThunkAppDispatch } from '../redux/store'
 
-const Logout: NextPage = () => {
+const Logout: NextPage = memo(() => {
   const user = useSelector((state) => state.user.currentUser)
-  const dispatch = useDispatch()
-  const { data, loading, error } = apiHooks.useLogoutUser(!!user)
-  user && dispatch(actions.logoutUser())
+  const dispatch: ThunkAppDispatch = useDispatch()
+  const router = useRouter()
+  const { data, loading, error } = apiHooks.useLogoutUser(user)
+  data?.token &&
+    user &&
+    (() => {
+      helpers.deleteToken()
+      dispatch(actions.clearDays())
+      dispatch(actions.setUnauthenticated()).then(() => router.push('/'))
+    })()
   if (loading) {
     return <Loader size="lg" center content="loading..." />
   } else if (error) {
@@ -28,6 +39,7 @@ const Logout: NextPage = () => {
         </FlexboxGrid.Item>
       </FlexboxGrid>
     )
-}
+})
 
+Logout.displayName = 'Logout'
 export default Logout
