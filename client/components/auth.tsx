3cd52bdf6082972bab52b064c73f7d4437a3b030
ghemsley <ghemsley@protonmail.ch>
2021-10-20 10:56:48 -0500

    Refactor auth component

--- client/components/auth.tsx
+++ client/components/auth.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useState } from 'react'
+import { FunctionComponent, memo, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Link from 'next/link'
 import { Loader } from 'rsuite'
@@ -6,27 +6,31 @@ import apiHooks from '../api'
 import { ThunkAppDispatch } from '../redux/store'
 import actions from '../redux/actions'
 import Redirect from './redirect'
+import { isUserObject } from '../models/user.guard'
+import { UserObject } from '../models/user'
 
-const Auth: FunctionComponent = ({ children }) => {
-  const currentUser = useSelector((state) => state.user.currentUser)
-  const [startFetch, setStartFetch] = useState(false)
-  const { data, error, loading } = apiHooks.useCheckAuth(startFetch)
-  const dispatch: ThunkAppDispatch = useDispatch()
-  if (!currentUser && !startFetch) {
-    setStartFetch(true)
-  }
-  if (!currentUser && data?.user) {
-    dispatch(actions.setAuthenticated(data.user)).then(() => {
-      setStartFetch(false)
-    })
-  }
-  return loading ? (
-    <Loader size="lg" center content="loading..." />
-  ) : currentUser ? (
-    <>{children}</>
+const Auth: FunctionComponent = memo(({ children }) => {
+  const [start, setStart] = useState(false)
+  const [done, setDone] = useState(false)
+  const currentUser = useSelector(state => state.user.currentUser)
+  useEffect(() => {
+    !currentUser && !start && !done && setStart(true)
+  }, [currentUser, start, done])
+  apiHooks.useCheckAuth(start && !done).then(data => {
+    setStart(false)
+    setDone(true)
+  })
+  return done ? (
+    currentUser ? (
+      <>{children}</>
+    ) : (
+      <Redirect to="/login" />
+    )
   ) : (
-    <Redirect to="/login" />
+    <Loader size="lg" center content="loading..." />
   )
-}
+})
+
+Auth.displayName = 'Auth'
 
 export default Auth
