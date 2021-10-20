fa2daf20286526d755b3fe141cb8cde5776a1ff9
ghemsley <ghemsley@protonmail.ch>
2021-10-20 10:58:33 -0500

    Refactor logout page

--- client/pages/logout.tsx
+++ client/pages/logout.tsx
@@ -1,44 +1,29 @@
 import type { NextPage } from 'next'
 import { useRouter } from 'next/dist/client/router'
-import { memo, useState } from 'react'
+import { memo, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { FlexboxGrid, Loader } from 'rsuite'
 import apiHooks from '../api'
 import helpers from '../api/helpers'
+import Auth from '../components/auth'
+import Redirect from '../components/redirect'
 import actions from '../redux/actions'
 import { ThunkAppDispatch } from '../redux/store'
 
 const Logout: NextPage = memo(() => {
-  const user = useSelector((state) => state.user.currentUser)
-  const dispatch: ThunkAppDispatch = useDispatch()
-  const router = useRouter()
-  const { data, loading, error } = apiHooks.useLogoutUser(user)
-  data?.token &&
-    user &&
-    (() => {
-      helpers.deleteToken()
-      dispatch(actions.clearDays())
-      dispatch(actions.setUnauthenticated()).then(() => router.push('/'))
-    })()
-  if (loading) {
-    return <Loader size="lg" center content="loading..." />
-  } else if (error) {
-    return (
-      <FlexboxGrid justify="center" align="middle">
-        <FlexboxGrid.Item colspan={100}>
-          <p>{JSON.stringify(error)}</p>
-        </FlexboxGrid.Item>
-      </FlexboxGrid>
-    )
-  } else
-    return (
-      <FlexboxGrid justify="center" align="middle">
-        <FlexboxGrid.Item colspan={100}>
-          <p>Logged out</p>
-          <p>{data && JSON.stringify(data)}</p>
-        </FlexboxGrid.Item>
-      </FlexboxGrid>
-    )
+  const [done, setDone] = useState(false)
+  const [start, setStart] = useState(false)
+  useEffect(() => {
+    !start && !done && setStart(true)
+  }, [start, done])
+  apiHooks.useLogoutUser(!done && start).then(data => setDone(true))
+  return done ? (
+    <Redirect to="/login" />
+  ) : (
+    <Auth>
+      <Loader size="lg" center content="logging out..." />
+    </Auth>
+  )
 })
 
 Logout.displayName = 'Logout'
