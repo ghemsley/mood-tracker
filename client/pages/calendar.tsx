4977aaddb0964e22c804d88ce9b6dc95d01a9bdc
ghemsley <ghemsley@protonmail.ch>
2021-10-20 10:58:15 -0500

    Refactor calendar page

--- client/pages/calendar.tsx
+++ client/pages/calendar.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
-import { useDispatch, useSelector } from 'react-redux'
-import { Calendar, FlexboxGrid, List, Loader, Panel, Popover, Whisper } from 'rsuite'
+import { useSelector } from 'react-redux'
+import { Calendar, List, Loader, Panel, Popover, Whisper } from 'rsuite'
 import { AiFillStar } from 'react-icons/ai'
 import { MdMood } from 'react-icons/md'
 import { FaHamburger } from 'react-icons/fa'
@@ -10,31 +10,35 @@ import { CgArrowDownO } from 'react-icons/cg'
 import { GiWaterDrop } from 'react-icons/gi'
 import { IoIosChatbubbles, IoMdJournal, IoIosMore } from 'react-icons/io'
 import { MdVideogameAsset } from 'react-icons/md'
-import { FunctionComponent } from 'react'
+import { FunctionComponent, memo, useEffect, useState } from 'react'
 import { DayObject } from '../models/day'
 import apiHooks from '../api'
 import styles from '../styles/listItem.module.scss'
-import actions from '../redux/actions'
 import { UserObject } from '../models/user'
-import { ThunkAppDispatch } from '../redux/store'
 import Auth from '../components/auth'
+import Redirect from '../components/redirect'
 
 const ListItem: FunctionComponent = ({ children }) => (
   <div className={styles.listItem}>{children}</div>
 )
 
-const CalendarPage: NextPage = () => {
-  const currentUser: UserObject | null | undefined = useSelector((state) => state.user.currentUser)
-  const days: DayObject[] = useSelector((state) => state.days)
-  const dispatch: ThunkAppDispatch = useDispatch()
-  const { data, error, loading } = apiHooks.useFetchDays(
-    { userId: currentUser?.id },
-    !!currentUser && days.length < 1
-  )
-  error && console.error(error)
-  if (data?.days && days.length < 1) {
-    dispatch(actions.createDays(data.days))
-  }
+const defined = (variable: any) => {
+  return typeof variable !== 'undefined' && variable !== null
+}
+
+const CalendarPage: NextPage = memo(() => {
+  const user: UserObject | null | undefined = useSelector(state => state.user.currentUser)
+  const days: DayObject[] = useSelector(state => state.days)
+  const [start, setStart] = useState(false)
+  const [done, setDone] = useState(false)
+  const [errors, setErrors] = useState<{ message: string }[] | undefined>(undefined)
+  useEffect(() => {
+    !!user && !start && !done && setStart(true)
+  }, [user, start, done])
+  apiHooks.useFetchDays(undefined, { userId: user?.id }, start && !done).then(data => {
+    start && setStart(false)
+    !done && setDone(true)
+  })
   const renderCell = (date: Date) => {
     const iconSize = 20
     const currentDay: DayObject | undefined = days.find((day: DayObject) => {
@@ -50,14 +54,14 @@ const CalendarPage: NextPage = () => {
           speaker={
             <Popover title={new Date(currentDay.createdAt as number).toLocaleDateString()}>
               <List>
-                {currentDay.rating && (
+                {defined(currentDay.rating) && (
                   <List.Item>
                     <ListItem>
                       <AiFillStar size={iconSize} /> Rating: {currentDay.rating}
                     </ListItem>
                   </List.Item>
                 )}
-                {currentDay.journal && (
+                {defined(currentDay.journal) && (
                   <Whisper
                     trigger="hover"
                     placement="right"
@@ -78,42 +82,42 @@ const CalendarPage: NextPage = () => {
                     </a>
                   </Whisper>
                 )}
-                {currentDay.mood && (
+                {defined(currentDay.mood) && (
                   <List.Item>
                     <ListItem>
                       <MdMood size={iconSize} /> Mood: {currentDay.mood}
                     </ListItem>
                   </List.Item>
                 )}
-                {currentDay.meals && (
+                {defined(currentDay.meals) && (
                   <List.Item>
                     <ListItem>
                       <FaHamburger size={iconSize} /> Meals eaten: {currentDay.meals}
                     </ListItem>
                   </List.Item>
                 )}
-                {currentDay.water && (
+                {defined(currentDay.water) && (
                   <List.Item>
                     <ListItem>
                       <GiWaterDrop size={iconSize} /> Hydration: {currentDay.water} cups
                     </ListItem>
                   </List.Item>
                 )}
-                {currentDay.people && (
+                {defined(currentDay.people) && (
                   <List.Item>
                     <ListItem>
                       <IoIosChatbubbles size={iconSize} /> People talked to: {currentDay.people}
                     </ListItem>
                   </List.Item>
                 )}
-                {currentDay.activities && (
+                {defined(currentDay.activities) && (
                   <List.Item>
                     <ListItem>
                       <MdVideogameAsset size={iconSize} /> Fun activities: {currentDay.water}
                     </ListItem>
                   </List.Item>
                 )}
-                {currentDay.meds && (
+                {defined(currentDay.meds) && (
                   <List.Item>
                     <ListItem>
                       <RiMedicineBottleLine size={iconSize} /> Took meds:{' '}
@@ -121,7 +125,7 @@ const CalendarPage: NextPage = () => {
                     </ListItem>
                   </List.Item>
                 )}
-                {currentDay.exercise && (
+                {defined(currentDay.exercise) && (
                   <List.Item>
                     <ListItem>
                       <BiWalk size={iconSize} /> Exercised: {currentDay.exercise ? 'yes' : 'no'}
@@ -146,15 +150,25 @@ const CalendarPage: NextPage = () => {
 
   return (
     <Auth>
-      {loading ? (
-        <Loader size="lg" center content="loading..." />
+      {user ? (
+        done ? (
+          days.length > 0 ? (
+            <Panel header="Calendar" collapsible defaultExpanded bordered>
+              <Calendar bordered renderCell={renderCell} />
+            </Panel>
+          ) : (
+            <p>There is no data to display</p>
+          )
+        ) : (
+          <Loader center size="lg" content="loading..." />
+        )
       ) : (
-        <Panel header="Calendar" collapsible defaultExpanded bordered>
-          <Calendar bordered renderCell={renderCell} />
-        </Panel>
+        <Redirect to="/login" />
       )}
     </Auth>
   )
-}
+})
+
+CalendarPage.displayName = 'Calendar'
 
 export default CalendarPage
