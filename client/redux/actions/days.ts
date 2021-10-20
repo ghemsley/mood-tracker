d2287bfc303dfd210d851d3211c0fb5d20cffc8c
ghemsley <ghemsley@protonmail.ch>
2021-10-20 10:59:42 -0500

    Refactor client redux actions

--- client/redux/actions/days.ts
+++ client/redux/actions/days.ts
@@ -1,12 +1,26 @@
-import { Day, DayObject } from '../../models/day'
+import { DayObject } from '../../models/day'
+import { ThunkAppDispatch } from '../store'
+
+export type DayActionType = {
+  type: 'CREATE_DAY' | 'CREATE_DAYS' | 'CLEAR_DAYS'
+  payload?: DayObject | DayObject[]
+}
 
 const days = {
-  createDay: (payload: DayObject) => (dispatch: any) =>
-    Promise.resolve(dispatch({ type: 'CREATE_DAY', payload })),
-  createDays: (payload: DayObject[]) => (dispatch: any) =>
-    Promise.resolve(dispatch({ type: 'CREATE_DAYS', payload })),
+  createDay:
+    (payload: DayObject) =>
+    (dispatch: ThunkAppDispatch): Promise<DayActionType> =>
+      Promise.resolve(dispatch({ type: 'CREATE_DAY', payload })),
 
-  clearDays: () => (dispatch: any) => Promise.resolve(dispatch({ type: 'CLEAR_DAYS' })),
+  createDays:
+    (payload: DayObject[]) =>
+    (dispatch: ThunkAppDispatch): Promise<DayActionType> =>
+      Promise.resolve(dispatch({ type: 'CREATE_DAYS', payload })),
+
+  clearDays:
+    () =>
+    (dispatch: ThunkAppDispatch): Promise<DayActionType> =>
+      Promise.resolve(dispatch({ type: 'CLEAR_DAYS' })),
 }
 
 export default days
--- client/redux/actions/users.ts
+++ client/redux/actions/users.ts
@@ -2,18 +2,23 @@ import actions from '.'
 import { UserObject } from '../../models/user'
 import { ThunkAppDispatch } from '../store'
 
-type UserActionType = {
+export type UserActionType = {
   type: 'SET_AUTHENTICATED' | 'SET_UNAUTHENTICATED' | 'UPDATE_USER'
   payload?: UserObject
 }
 
 const users = {
-  setAuthenticated: (payload: UserObject) => (dispatch: ThunkAppDispatch) =>
-    Promise.resolve(dispatch({ type: 'SET_AUTHENTICATED', payload })),
-  setUnauthenticated: () => (dispatch: ThunkAppDispatch) =>
-    Promise.resolve(dispatch({ type: 'SET_UNAUTHENTICATED' })).then(() =>
-      dispatch(actions.clearDays())
-    ),
+  setAuthenticated:
+    (payload: UserObject) =>
+    (dispatch: ThunkAppDispatch): Promise<UserActionType> =>
+      Promise.resolve(dispatch({ type: 'SET_AUTHENTICATED', payload })),
+
+  setUnauthenticated:
+    () =>
+    (dispatch: ThunkAppDispatch): Promise<UserActionType> =>
+      Promise.resolve(dispatch({ type: 'SET_UNAUTHENTICATED' })).then(action =>
+        dispatch(actions.clearDays()).then(() => action)
+      ),
 }
 
 export default users
