aed6cc34eedaf06fecb0996e65cbfa33b5d92761
ghemsley <ghemsley@protonmail.ch>
2021-10-27 16:20:10 -0500

    Refactor user api hooks

--- client/api/users.ts
+++ client/api/users.ts
@@ -1,221 +1,151 @@
-import { request, gql } from 'graphql-request'
-import { useDispatch } from 'react-redux'
-import useSWR from 'swr'
-import constants from '../constants'
-import actions from '../redux/actions'
-import { ThunkAppDispatch } from '../redux/store'
-import helpers from './helpers'
-import type { UserObject } from '../models/user'
-import { isUserObject } from '../models/user.guard'
 import {
   Dispatch,
   MutableRefObject,
   SetStateAction,
   useCallback,
-  useEffect,
   useMemo,
   useRef,
+  useState,
 } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useMount, useMountedState, usePromise, useUpdateEffect } from 'react-use'
+import type { UserObject } from '../models/user'
+import { isUserObject } from '../models/user.guard'
+import actions from '../redux/actions'
+import { ThunkAppDispatch } from '../redux/store'
+import helpers, { AuthenticationType, ErrorType } from './helpers'
 
-export type ErrorType = {
-  errors: [{ message: string }]
-}
-
-export type AuthenticationType = {
-  user: UserObject
-  token: string
-}
+export type HookReturnType = [(AuthenticationType & ErrorType) | null, ErrorType['errors'] | null]
 
 const userHooks = {
-  useCheckAuth: async (
-    start: boolean,
-    started: MutableRefObject<boolean>,
-    unmounting: MutableRefObject<boolean>
-  ): Promise<(AuthenticationType & ErrorType) | null> => {
+  useCheckAuth: (): HookReturnType => {
+    const user = useSelector(state => state.user.currentUser)
+    const [data, setData] = useState<(AuthenticationType & ErrorType) | null>(null)
+    const [errors, setErrors] = useState<ErrorType['errors'] | null>(null)
     const dispatch: ThunkAppDispatch = useDispatch()
+    const mounted = usePromise()
+    const isMounted = useMountedState()
     const run = useCallback(async () => {
-      if (start && !started.current && !unmounting.current) {
-        started.current = true
-        const authorize = (url: string) =>
-          helpers.fetcher(url, 'GET', true) as Promise<AuthenticationType & ErrorType>
-        const data = await authorize('/authorize')
-        if (typeof data?.token === 'string') {
-          helpers.setToken(data.token)
-        }
-        if (isUserObject(data?.user)) {
-          await dispatch(actions.setAuthenticated(data.user))
-        } else {
-          console.log('error', data)
-        }
-        return data
-      } else return null
-    }, [start, started, dispatch, unmounting])
-    return start && !started.current && !unmounting.current ? await run() : null
+      const authorize = (url: string) =>
+        helpers.fetcher(url, 'GET', true) as Promise<AuthenticationType & ErrorType>
+      const data = await mounted(authorize('/authorize'))
+      if (typeof data?.token === 'string') {
+        helpers.setToken(data.token)
+      }
+      if (isUserObject(data?.user)) {
+        if (!user && isMounted()) await mounted(dispatch(actions.setAuthenticated(data.user)))
+        if (isMounted()) setData(data)
+      } else {
+        console.log('error', data)
+        helpers.deleteToken()
+        if (isMounted()) await mounted(dispatch(actions.setUnauthenticated()))
+        if (isMounted()) setErrors(data?.errors ? data.errors : null)
+      }
+    }, [user, isMounted, dispatch, mounted])
+    useMount(() => {
+      if (isMounted()) mounted(run())
+    })
+    return [data, errors]
   },
   useSignupUser: async (
     email: string,
     password: string,
     confirmPassword: string,
     start: boolean,
-    done: boolean,
-    started: MutableRefObject<boolean>,
-    unmounting: MutableRefObject<boolean>,
-    errorsCallback: Dispatch<SetStateAction<{ message: string }[]>>
-  ): Promise<(AuthenticationType & ErrorType) | null> => {
+    setErrors: Dispatch<ErrorType['errors'] | null>
+  ): Promise<HookReturnType> => {
+    const [data, setData] = useState<(AuthenticationType & ErrorType) | null>(null)
     const dispatch: ThunkAppDispatch = useDispatch()
-    const run = useCallback(async () => {
-      if (start && !done && !started.current && !unmounting.current) {
-        started.current = true
-        const signupUser = (url: string) =>
-          helpers.fetcher(url, 'POST', false, { email, password, confirmPassword })
-        const data = (await signupUser('/signup')) as AuthenticationType & ErrorType
-        if (typeof data?.token === 'string') {
-          helpers.setToken(data.token)
-        }
-        if (isUserObject(data?.user) && !unmounting.current) {
-          await dispatch(actions.setAuthenticated(data.user))
-        } else {
-          console.log('error', data)
-          if (!unmounting.current) errorsCallback(data?.errors)
-        }
-        return data
-      } else return null
-    }, [
-      start,
-      done,
-      started,
-      email,
-      password,
-      confirmPassword,
-      unmounting,
-      dispatch,
-      errorsCallback,
-    ])
-    return start && !done && !started.current && !unmounting.current ? await run() : null
+    const mounted = usePromise()
+    const isMounted = useMountedState()
+    const run = useCallback(async (): Promise<
+      [(AuthenticationType & ErrorType) | null, ErrorType['errors'] | null]
+    > => {
+      const signupUser = (url: string) =>
+        helpers.fetcher(url, 'POST', false, { email, password, confirmPassword })
+      const data = (await mounted(signupUser('/signup'))) as AuthenticationType & ErrorType
+      if (typeof data?.token === 'string') {
+        helpers.setToken(data.token)
+      }
+      if (isUserObject(data?.user)) {
+        if (isMounted()) await mounted(dispatch(actions.setAuthenticated(data.user)))
+      } else {
+        console.log('error', data)
+      }
+      return [data, data?.errors ? data.errors : null]
+    }, [email, password, confirmPassword, isMounted, mounted, dispatch])
+    useUpdateEffect(() => {
+      if (start && isMounted())
+        mounted(
+          run().then(([data, errors]) => {
+            if (isMounted()) setData(data)
+            if (isMounted()) setErrors(errors)
+          })
+        )
+    }, [start, isMounted, mounted, run, setErrors])
+    return [data, data?.errors ? data.errors : null]
   },
   useLoginUser: async (
     email: string,
     password: string,
     start: boolean,
-    started: MutableRefObject<boolean>,
-    unmounting: MutableRefObject<boolean>,
-    errorsCallback: Dispatch<SetStateAction<{ message: string }[]>>
-  ): Promise<(AuthenticationType & ErrorType) | null> => {
+    setErrors: Dispatch<ErrorType['errors'] | null>
+  ): Promise<HookReturnType> => {
+    const [data, setData] = useState<(AuthenticationType & ErrorType) | null>(null)
     const dispatch: ThunkAppDispatch = useDispatch()
-    const run = useCallback(async () => {
-      if (start && !started.current && !unmounting.current) {
-        started.current = true
-        console.log('running')
-        const loginUser = (url: string) => helpers.fetcher(url, 'POST', false, { email, password })
-        const data = (await loginUser('/login')) as AuthenticationType & ErrorType
-        if (typeof data?.token === 'string') {
-          helpers.setToken(data.token)
-        }
-        if (isUserObject(data?.user) && !unmounting.current) {
-          await dispatch(actions.setAuthenticated(data.user))
-        } else {
-          console.log('error', data)
-          if (!unmounting.current) errorsCallback(data?.errors)
-        }
-        return data
-      } else {
-        return null
+    const mounted = usePromise()
+    const isMounted = useMountedState()
+    const run = useCallback(async (): Promise<
+      [(AuthenticationType & ErrorType) | null, ErrorType['errors'] | null]
+    > => {
+      const loginUser = (url: string) => helpers.fetcher(url, 'POST', false, { email, password })
+      const data = (await mounted(loginUser('/login'))) as AuthenticationType & ErrorType
+      if (typeof data?.token === 'string') {
+        helpers.setToken(data.token)
       }
-    }, [start, started, email, password, unmounting, dispatch, errorsCallback])
-    return start && !started.current && !unmounting.current ? await run() : null
+      if (isUserObject(data?.user)) {
+        if (isMounted()) await mounted(dispatch(actions.setAuthenticated(data.user)))
+      } else {
+        console.log('error', data)
+      }
+      return [data, data?.errors ? data.errors : null]
+    }, [email, password, dispatch, isMounted, mounted])
+    useUpdateEffect(() => {
+      if (start && isMounted())
+        mounted(
+          run().then(([data, errors]) => {
+            if (isMounted()) {
+              setErrors(errors)
+              setData(data)
+            }
+          })
+        )
+    }, [start, isMounted, mounted, run])
+    return [data, data?.errors ? data.errors : null]
   },
-  useLogoutUser: async (
-    start: boolean,
-    started: MutableRefObject<boolean>,
-    unmounting: MutableRefObject<boolean>
-  ): Promise<{ token: string } | ErrorType | null> => {
+  useLogoutUser: (): [{ token: string } | ErrorType | null, ErrorType['errors'] | null] => {
     const dispatch: ThunkAppDispatch = useDispatch()
+    const mounted = usePromise()
+    const isMounted = useMountedState()
+    const [data, setData] = useState<{ token: string } | ErrorType | null>(null)
+    const [errors, setErrors] = useState<ErrorType['errors'] | null>(null)
     const run = useCallback(async () => {
-      if (start && !started.current && !unmounting.current) {
-        started.current = true
-        const logoutUser = (url: string) => helpers.fetcher(url, 'GET', true)
-        const data = (await logoutUser('/logout')) as { token: string } & ErrorType
-        if (typeof data?.token === 'string') {
-          console.log('token', data.token)
-        } else {
-          console.log(data)
-        }
-        await dispatch(actions.setUnauthenticated())
-        return data
-      } else return null
-    }, [start, started, unmounting, dispatch])
-    return start && !started.current && !unmounting.current ? await run() : null
-  },
-  useFetchUserById: async (
-    id: number,
-    start: boolean
-  ): Promise<{ data: UserObject | { errors: { message: string }[] } }> => {
-    const dispatch: ThunkAppDispatch = useDispatch()
-    const result = useMemo(async () => {
-      if (start) {
-        const data = await helpers.fetchGraphQL('user', undefined, { id })
-        console.log('data', data)
-        return data
-      } else return null
-    }, [id, start])
-    return result
-  },
-  useFetchUserByEmail: (
-    email: string,
-    start: boolean
-  ): { data: any; error: any; loading: boolean } => {
-    const fetchUserByEmail = async (url: string) =>
-      request(
-        url,
-        gql`{
-        user(email: "${email}") {
-          id
-          email
-          admin
-          enabled
-          createdAt
-          updatedAt
-        }
-      }`,
-        null,
-        helpers.auth()
-      )
-    const { data, error, isValidating } = useSWR(
-      start ? constants.API + '/api' : null,
-      fetchUserByEmail
-    )
-    return {
-      data: data && JSON.parse(data),
-      error,
-      loading: isValidating,
-    }
-  },
-  useFetchUsers: (
-    args?: UserObject,
-    start?: boolean
-  ): { data: any; error: any; loading: boolean } => {
-    const fetchUsers = async (url: string) =>
-      request(
-        url,
-        gql`{
-        users${helpers.stringifyArgs(args)} {
-          id
-          email
-          admin
-          enabled
-          createdAt
-          updatedAt
-        }
-      }`,
-        null,
-        helpers.auth()
-      )
-    const { data, error, isValidating } = useSWR(start ? constants.API + '/api' : null, fetchUsers)
-    return {
-      data: data && JSON.parse(data),
-      error,
-      loading: isValidating,
-    }
+      const logoutUser = (url: string) => helpers.fetcher(url, 'GET', true)
+      const data = (await mounted(logoutUser('/logout'))) as { token: string } & ErrorType
+      helpers.deleteToken()
+      if (isMounted()) await mounted(dispatch(actions.setUnauthenticated()))
+      if (typeof data?.token === 'string') {
+        console.log('token', data.token)
+        if (isMounted()) setData(data)
+      } else {
+        console.log('error', data)
+        if (isMounted()) setErrors(data?.errors ? data.errors : null)
+      }
+    }, [isMounted, mounted, dispatch])
+    useMount(() => {
+      if (isMounted()) mounted(run())
+    })
+    return [data, errors]
   },
 }
 
