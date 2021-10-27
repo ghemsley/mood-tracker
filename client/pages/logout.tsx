267dea3943180ad7f63e1677f3bf510e16db24a6
ghemsley <ghemsley@protonmail.ch>
2021-10-27 16:21:50 -0500

    Simplify logout page

--- client/pages/logout.tsx
+++ client/pages/logout.tsx
@@ -1,38 +1,15 @@
 import type { NextPage } from 'next'
-import { useRouter } from 'next/dist/client/router'
-import { memo, useEffect, useLayoutEffect, useRef, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { FlexboxGrid, Loader } from 'rsuite'
+import { memo } from 'react'
+import { Loader } from 'rsuite'
 import apiHooks from '../api'
-import helpers from '../api/helpers'
-import Auth from '../components/auth'
 import Redirect from '../components/redirect'
-import actions from '../redux/actions'
-import { ThunkAppDispatch } from '../redux/store'
 
 const Logout: NextPage = memo(() => {
-  const [done, setDone] = useState(false)
-  const [start, setStart] = useState(false)
-  const started = useRef(false)
-  const unmounting = useRef(false)
-  apiHooks.useLogoutUser(!done && start, started, unmounting).then(data => {
-    if (!unmounting.current) {
-      start && setStart(false)
-      !done && setDone(true)
-    }
-  })
-  useLayoutEffect(() => {
-    if (!start && !done && !started.current && !unmounting.current) {
-      !start && setStart(true)
-    }
-    return () => {
-      unmounting.current = true
-    }
-  }, [start, done])
-  return (
-    <Auth>
-      {done ? <Redirect to="/login" /> : <Loader size="lg" center content="logging out..." />}
-    </Auth>
+  const [data, errors] = apiHooks.useLogoutUser()
+  return data || errors ? (
+    <Redirect to="/login" />
+  ) : (
+    <Loader size="lg" center content="logging out..." />
   )
 })
 
