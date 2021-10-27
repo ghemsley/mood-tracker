1ff9cc2f7fcc6d9dddfc4fac38922feaacce7106
ghemsley <ghemsley@protonmail.ch>
2021-10-27 16:22:16 -0500

    Refactor days reducer

--- client/redux/reducers/days.ts
+++ client/redux/reducers/days.ts
@@ -1,29 +1,35 @@
+import { StateFromReducersMapObject } from 'redux'
 import { DayObject } from '../../models/day'
 
+type DaysStateType = {
+  days: DayObject[] | null
+}
 const days = (
-  state: DayObject[] = [],
+  state: DaysStateType = { days: null },
   action: {
     type: 'CREATE_DAY' | 'CREATE_DAYS' | 'UPDATE_DAY' | 'DELETE_DAY' | 'CLEAR_DAYS'
-    payload?: DayObject | DayObject[]
+    payload?: DayObject[]
   }
-) => {
+): DaysStateType => {
   switch (action.type) {
-    case 'CREATE_DAY':
-      return action.payload ? [...state, action.payload as DayObject] : state
     case 'CREATE_DAYS':
-      return action.payload ? [...state, ...(action.payload as DayObject[])] : state
+      return state.days && action.payload
+        ? { days: [...state.days, ...action.payload] }
+        : { days: action?.payload ? action.payload : null }
     case 'UPDATE_DAY':
       return (() => {
-        const day = action.payload as DayObject
-        return day ? [...state.filter((existing) => existing.id !== day.id), day] : state
+        const day = action?.payload && action.payload[0]
+        return day && state.days
+          ? { days: [...state.days.filter(existing => existing.id !== day.id), day] }
+          : state
       })()
     case 'DELETE_DAY':
       return (() => {
-        const { id } = action?.payload as DayObject
-        return id ? state.filter((day) => day.id !== id) : state
+        const id = action?.payload && action.payload[0].id
+        return id && state.days ? { days: state.days.filter(day => day.id !== id) } : state
       })()
     case 'CLEAR_DAYS':
-      return []
+      return { days: null }
 
     default:
       return state
