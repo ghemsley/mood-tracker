cea2fafa56a053cf6d5559c7b8c498066a6d7513
ghemsley <ghemsley@protonmail.ch>
2021-10-27 16:20:00 -0500

    Refactor day api hooks

--- client/api/days.ts
+++ client/api/days.ts
@@ -1,114 +1,97 @@
-import { DayObject } from './../models/day'
-import { request, gql } from 'graphql-request'
-import useSWR from 'swr'
-import constants from '../constants'
-import helpers from './helpers'
-import { useDispatch } from 'react-redux'
-import { useEffect, useRef } from 'react'
+import { useCallback, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useMount, useMountedState, usePromise } from 'react-use'
 import { isDayObject } from '../models/day.guard'
+import { UserObject } from '../models/user'
 import actions from '../redux/actions'
 import { ThunkAppDispatch } from '../redux/store'
+import { DayObject } from './../models/day'
+import helpers, { DaysType, ErrorType } from './helpers'
 
 const dayHooks = {
-  useFetchDayById: (
-    id: number,
-    start: boolean
-  ): { data: { day: DayObject } | undefined; error: any; loading: boolean } => {
-    const fetchDayById = async (url: string) =>
-      request(
-        url,
-        gql`
-          {
-            day(id: ${id}) {
-              id
-              userId
-              rating
-              mood
-              meals
-              water
-              people
-              activities
-              exercise
-              meds
-              journal
-              createdAt
-              updatedAt
-            }
-          }
-        `,
-        null,
-        helpers.auth()
-      )
-    const { data, error, isValidating } = useSWR(
-      start ? constants.API + '/api' : null,
-      fetchDayById
-    )
-    return {
-      data: data && JSON.parse(data),
-      error: error,
-      loading: isValidating,
-    }
-  },
-  useFetchDaysByUserId: (
-    userId: number,
-    start: boolean
-  ): { data: { days: DayObject[] } | undefined; error: any; loading: boolean } => {
-    const fetchDayById = async (url: string) =>
-      request(
-        url,
-        gql`
-          {
-            days(userId: ${userId}) {
-              id
-              userId
-              rating
-              mood
-              meals
-              water
-              people
-              activities
-              exercise
-              meds
-              journal
-              createdAt
-              updatedAt
-            }
-          }
-        `,
-        null,
-        helpers.auth()
-      )
-    const { data, error, isValidating } = useSWR(
-      start ? constants.API + '/api' : null,
-      fetchDayById
-    )
-    return {
-      data: data && JSON.parse(data),
-      error: error,
-      loading: isValidating,
-    }
-  },
-  useFetchDays: async (fields?: string[], args?: DayObject, start?: boolean): Promise<any> => {
+  useFetchDay: (
+    fields?: string[],
+    args?: DayObject
+  ): [DaysType['days'] | null, ErrorType['errors'] | null | undefined] => {
+    const [errors, setErrors] = useState<{ message: string }[] | null | undefined>(null)
+    const [days, setDays] = useState<DaysType['days'] | null>(null)
     const dispatch: ThunkAppDispatch = useDispatch()
-    const started = useRef(false)
-    const result = useRef<{ days?: DayObject[] } | { errors?: [{ message: string }] } | null>({})
-    useEffect(() => {
-      const run = async () => {
-        if (start && !started.current) {
-          started.current = true
-          const data = await JSON.parse(await helpers.fetchGraphQL('days', fields, args))
-          if (data?.days && data.days.length > 0 && isDayObject(data.days[0])) {
-            await dispatch(actions.createDays(data.days))
-            result.current = data
-          } else {
-            console.log('error', data)
-            result.current = data
-          }
-        } else result.current = null
+    const mounted = usePromise()
+    const isMounted = useMountedState()
+    const run = useCallback(async () => {
+      const data = (await mounted(helpers.fetchGraphQL('days', fields, args))) as
+        | (DaysType & ErrorType)
+        | null
+      if (data?.days) {
+        if (isMounted()) {
+          setDays(data.days)
+          await mounted(dispatch(actions.createDays(data.days)))
+        }
+      } else {
+        console.log('error', data)
+        if (isMounted()) setErrors(data?.errors)
       }
-      run()
-    }, [start, args, fields, dispatch])
-    return result.current
+      return data
+    }, [args, fields, isMounted, dispatch, mounted])
+    useMount(() => {
+      if (isMounted()) mounted(run())
+    })
+    return [days, errors]
+  },
+  useFetchDaysByUser: (
+    fields?: string[],
+    args?: DayObject
+  ): [DaysType['days'] | null, ErrorType['errors'] | null | undefined] => {
+    const user = useSelector(state => state.user.currentUser) as UserObject | null
+    const [errors, setErrors] = useState<{ message: string }[] | null | undefined>(null)
+    const [days, setDays] = useState<DaysType['days'] | null>(null)
+    const dispatch: ThunkAppDispatch = useDispatch()
+    const mounted = usePromise()
+    const isMounted = useMountedState()
+    const run = useCallback(async () => {
+      const data = (await mounted(
+        helpers.fetchGraphQL('days', fields, { ...args, userId: user?.id })
+      )) as (DaysType & ErrorType) | null
+      if (data?.days && isMounted()) {
+        setDays(data.days)
+        await mounted(dispatch(actions.createDays(data.days)))
+      } else {
+        console.log('error', data)
+        if (isMounted()) setErrors(data?.errors)
+      }
+      return data
+    }, [args, fields, user?.id, isMounted, dispatch, mounted])
+    useMount(() => {
+      if (user && isMounted()) mounted(run())
+    })
+    return [days, errors]
+  },
+  useFetchDays: (
+    fields?: string[],
+    args?: DayObject
+  ): [DaysType['days'] | null, ErrorType['errors'] | null | undefined] => {
+    const [errors, setErrors] = useState<{ message: string }[] | null | undefined>(null)
+    const [days, setDays] = useState<DaysType['days'] | null>(null)
+    const dispatch: ThunkAppDispatch = useDispatch()
+    const mounted = usePromise()
+    const isMounted = useMountedState()
+    const run = useCallback(async () => {
+      const data = (await mounted(helpers.fetchGraphQL('days', fields, args))) as
+        | (DaysType & ErrorType)
+        | null
+      if (data?.days && isMounted()) {
+        setDays(data.days)
+        await dispatch(actions.createDays(data.days))
+      } else {
+        console.log('error', data)
+        if (isMounted()) setErrors(data?.errors)
+      }
+      return data
+    }, [args, fields, isMounted, dispatch, mounted])
+    useMount(() => {
+      if (isMounted()) mounted(run())
+    })
+    return [days, errors]
   },
 }
 
